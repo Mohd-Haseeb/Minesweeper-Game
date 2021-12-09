f46845b24ffc1d95742bb759d07b6f377123ad44
Mohd Haseeb <mohd.haseeb440@gmail.com>
2021-12-09 15:32:06 +0530

    Added algo part 1

--- cells.py
+++ cells.py
@@ -1,25 +1,39 @@
 from tkinter import  Button
+import random
+import settings
 
 class Cell:
-    def __init__(self, is_mine = False) -> None:
+    all =[]
+    def __init__(self, x, y, is_mine = False) -> None:
         self.is_mine = is_mine
+        self.x = x
+        self.y = y
         self.cell_btn_object = None
+
+        Cell.all.append(self)
     
-    # def create_btn_object(self, location):
-    #     btn = Button(location, text='click')
 
-    #     btn.bind('<Button-1>', self.left_click_actions ) # Left Click
-    #     btn.bind('<Button-3>', self.right_click_actions ) # Right Click
-
-    #     self.cell_btn_object = btn
 
     def create_btn_object(self, location):
-        btn = Button(location, text="click")
+        btn = Button(location,width=12, height=4, text=f"{self.x},{self.y}")
 
         btn.bind('<Button-1>', self.left_click_actions ) # Left Click
         btn.bind('<Button-3>', self.right_click_actions ) # Right Click
 
         self.cell_btn_object = btn
+
+    
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(Cell.all, settings.MINES_COUNT)
+
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    
+    def __repr__(self) -> str:
+        return f"Cell({self.x},{self.y})"
+
 
 
 
--- main.py
+++ main.py
@@ -52,10 +52,15 @@ center_frame.place(x=utils.widtht_prcnt(25),y=utils.height_prcnt(25))
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
-        c = Cell()
+        c = Cell(x,y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid(column=x, row=y)
 
 
+Cell.randomize_mines()
+
+print(Cell.all)
+for mine in Cell.all:
+    print(mine.is_mine)
 # run the window
 root.mainloop()
